c67106cb5d7664ca54e10ca95dd296f5cb7ed37b
최상현 <yuio1126@gmail.com>
2021-07-19 16:59:29 +0900

    ~ HTTP response: html data

---
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.response;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ResponseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    //Content-type: text/html;charset=utf-8
+    resp.setContentType("text/html");
+    resp.setCharacterEncoding("utf-8");
+
+    final PrintWriter writer = resp.getWriter();
+    writer.println("<html>");
+    writer.println("<body>");
+    writer.println("  <h1>안녕?<h1>");
+    writer.println("</body>");
+    writer.println("</html>");
+  }
+}
