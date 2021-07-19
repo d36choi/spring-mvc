fd60ecb483fa92e45035217a94af922a7d539901
최상현 <yuio1126@gmail.com>
2021-07-19 15:00:36 +0900

    ~ Hello Servlet
    - logging.level.org.apache.coyote.http11=debug
    - @ServletComponentScan

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// springboot 환경에서 제공하는 서블릿 자동 등록위한 어노테이션
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    System.out.println("HelloServlet.service");
+    System.out.println("req = " + req);
+    //req = org.apache.catalina.connector.RequestFacade@2dcebe8c <- tomcat의 request 구현체
+    System.out.println("resp = " + resp);
+
+    final String name = req.getParameter("name");
+    System.out.println("name = " + name);
+    // query parameter
+
+    resp.setContentType("text/plain");
+    resp.setCharacterEncoding("utf-8");
+    // 옛날이나 EUC-KR
+    resp.getWriter().write("hello" + name);
+  }
+}
